8f4ac1c9fc00228a45ab78195efd2faa43453027
John Mark <dr3adlokk@gmail.com>
2018-06-06 13:19:30 -0700

    pics dont work

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+}touc
 
 // Send every request to the React app
 // Define any API routes before this runs
